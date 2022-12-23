048df5526960aca1b0afb026b61e0292fa528da8
Guilherme Augusto <guilhermeasn@yahoo.com.br>
2022-12-23 12:45:22 -0300

    pushing megasena

---
+++ src/helpers.ts
@@ -0,0 +1,36 @@
+import axios from 'axios';
+import { writeFileSync } from 'fs';
+import { join } from 'path';
+
+const API = 'http://servicebus2.caixa.gov.br/portaldeloterias/api';
+
+export type Raffle = { [key in number]: number[] };
+export type Result = Raffle | null;
+export type Lottery = 'megasena' | 'lotofacil' | 'quina' | 'duplasena' | 'lotomania';
+
+export function writeRaffle(lottery : Lottery, raffle : Raffle) {
+
+    writeFileSync(
+        join('data', lottery + '.json'),
+        JSON.stringify(raffle)
+    );
+
+}
+
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
+
+export async function getResult(lottery : Lottery, number ?: number) : Promise<Result> {
+
+    try {
+
+        const http = await axios.get(`${ API }/${ lottery }/${ number?.toString() || '' }`);
+        return { [http.data.numero]: http.data.dezenasSorteadasOrdemSorteio }
+
+    } catch(error) {
+
+        console.error(error);
+        return null;
+
+    }
+
+}
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,28 @@
-const t : string = 'test';
-console.log(t);
+import { getResult, Raffle, Result, writeRaffle } from "./helpers";
+import megasena from '../data/megasena.json';
+
+async function main() {
+
+    let raffle : Raffle = {};
+    let result : Result = null;
+    let count  : number = 0;
+
+    while(true) if(!(++count in megasena)) {
+
+        result = await getResult('megasena', count);
+        if(!result) break;
+
+        console.log('Added raffle: ' + Object.entries(result)[0]);
+
+        raffle = {
+            ...raffle,
+            ...(result ?? {})
+        };
+
+    };
+
+    writeRaffle('megasena', { ...megasena, ...raffle } as unknown as Raffle);
+
+}
+
+main();
